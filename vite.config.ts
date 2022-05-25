6331f0241bf83af901677c836acf0b2fd23cc8d3
Jake Hamilton <jake.hamilton@hey.com>
2022-05-25 03:39:11 -0700

    fix: resolve vite startup issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,15 @@
+/// <reference path="node_modules/vitest/node.d.ts" />
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+	build: {},
+	server: {
+		watch: {
+			ignored: ["**/.direnv"],
+		},
+	},
+	test: {
+		exclude: [".direnv"],
+	},
+});
